4add3958143810e7dec474e1af85ba463bc7869d
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-07-05 15:10:26 -0700

    stuck

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -15,32 +15,84 @@ export function Subscribe() {
     return (
         
         <SubscribeForm 
+        id = 'subscribe'
+        method = 'post'
+        enctype = 'text/plain'
         onsubmit = {e => handleSubmit(e)}
         >
-            {console.log(user)}
-            <label htmlFor = 'name'>
-                <input
-                name = 'name'
-                placeholer = 'Enter Name Here'
-                value = {user.name}
-                onChange = {e => handleChange(e)}
-                required
-                />
-            </label>
-            <label htmlFor = 'email'>
-                <input
-                name = 'email'
-                placeholer = 'Enter Email Here'
-                value = {user.email}
-                onChange = {e => handleChange(e)}
-                required
-                />
-            </label>
-            <button type = 'submit'>Subscribe</button>
+            <div className = 'subscribe-header'>
+                <h3>Let's Stay in Touch</h3>
+                <p>Sign Up to Receive News & Updates</p>
+            </div>
+            <div className = 'label-container'>
+                {console.log(user)}
+                <label htmlFor = 'name'>
+                    <input
+                    type = 'text'
+                    name = 'name'
+                    placeholder = 'Enter Name'
+                    value = {user.name}
+                    onChange = {e => handleChange(e)}
+                    required
+                    />
+                </label>
+                <label htmlFor = 'email'>
+                    <input
+                    type = 'email'
+                    name = 'email'
+                    placeholder = 'Enter Email'
+                    value = {user.email}
+                    onChange = {e => handleChange(e)}
+                    required
+                    />
+                </label>
+            </div>
+            <div className = 'button-container'>
+                <button type = 'submit'>Subscribe</button>
+            </div>
         </SubscribeForm>
     );
 }
 
 const SubscribeForm = styled.form `
+    
 
+    background-color: lightgray;
+    
+    .subscribe-header {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        h3 {
+            margin-bottom: 0;
+        }
+    }
+    .label-container {
+        display: flex;
+        justify-content: center;
+        label {
+            margin: 0 3% 1% 3%;
+            input {
+                padding: 9% 9% 9% 6%;
+                font-size: 1.6rem;
+                background: transparent;
+                border: 1px solid black;
+            }
+        }
+    }
+
+    .button-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        button {
+            font-size: 1.6rem;
+            padding: 2% 5%;
+            background: transparent;
+            border: 1px solid black;
+            &:hover {
+
+            }
+        }
+    }
 `
